7224c9a90f0a9ced92b0af3ff4d2a06764357a34
Thamiris Adriano <adrianosthamiris@gmail.com>
2022-11-16 11:15:09 -0300

    api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import React, { useEffect, useState } from "react";
-import api from "./services/api";
+import React from 'react';
+import Api from './services/api'
 
-export default function App() {
-  const [dados, setDados] = useState();
-
-  useEffect(() => {
-    api
-      .get("https://digimon-api.vercel.app/api/digimon")
-      .then((response) => setDados(response))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
-
+function App() {
   return (
-    <div className="App">
-      <h1>API Consumer React</h1>
-      <p>Nome: {dados?.img}</p>
-      <p>Gênero: {dados?.name}</p>
-      <p>Sinopse: {dados?.level}</p>
+    <div>
+      <Api />
     </div>
   );
 }
+export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,41 @@
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-const api = axios.create({
-  baseURL: "https://digimon-api.vercel.app/api/digimon",
-});
+const Api = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://digimon-api.vercel.app/api/digimon")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  });
 
-export default api;
+  return (
+    <table
+      style={{
+        border: "1px solid black",
+        textAlign: "center",
+        margin: "auto",
+        padding: "20px",
+        width: "50%",
+      }}
+    >
+      <tr>
+        <th>Foto</th>
+        <th>Nome</th>
+        <th>Nível</th>
+      </tr>
+      {users.map((user) => (
+        <tr>
+          <td>
+            <img width={50} src={user.img}></img>
+          </td>
+          <td>{user.name}</td>
+          <td>{user.level}</td>
+        </tr>
+      ))}
+    </table>
+  );
+};
+
+export default Api;
